63b4ba4a7274caa354adc42bd9cdbc5a3af7c589
sakamata0 <iskanderboughnimi@gmail.com>
2026-03-17 02:56:00 +0100

    Added domain section

--- Frontend/src/app/features/landing-page/landing-page.ts
+++ Frontend/src/app/features/landing-page/landing-page.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
-import { HeroSection } from './components/hero-section/hero-section';
+import { HeroSection } from './hero-section/hero-section';
 import { FeaturesSection } from './components/features-section/features-section';
+import { DomainSection } from './components/domain-section/domain-section';
 
 @Component({
   selector: 'app-landing-page',
   imports: [
     Header,
     HeroSection,
-    FeaturesSection
+    FeaturesSection,
+    DomainSection
 ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
