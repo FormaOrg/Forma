fb765c136ed6d8f2de1bafde63aa05d06e8a0e86
sakamata0 <iskanderboughnimi@gmail.com>
2026-03-17 02:15:24 +0100

    Fixed Componnets Folder

--- Frontend/src/app/features/landing-page/landing-page.ts
+++ Frontend/src/app/features/landing-page/landing-page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
-import { HeroSection } from './hero-section/hero-section';
-import { FeaturesSection } from './features-section/features-section';
+import { HeroSection } from './components/hero-section/hero-section';
+import { FeaturesSection } from './components/features-section/features-section';
 
 @Component({
   selector: 'app-landing-page',
