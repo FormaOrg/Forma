d4f1d442530788860b728815a568257b3655d72b
sakamata0 <iskanderboughnimi@gmail.com>
2026-03-17 07:58:56 +0100

    Added Features, Domain & FAQ Sections

--- Frontend/src/app/features/landing-page/landing-page.ts
+++ Frontend/src/app/features/landing-page/landing-page.ts
@@ -3,6 +3,7 @@ import { Header } from '../../shared/header/header';
 import { HeroSection } from './hero-section/hero-section';
 import { FeaturesSection } from './components/features-section/features-section';
 import { DomainSection } from './components/domain-section/domain-section';
+import { FaqSection } from './components/faq-section/faq-section';
 
 @Component({
   selector: 'app-landing-page',
@@ -10,7 +11,8 @@ import { DomainSection } from './components/domain-section/domain-section';
     Header,
     HeroSection,
     FeaturesSection,
-    DomainSection
+    DomainSection,
+    FaqSection
 ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
