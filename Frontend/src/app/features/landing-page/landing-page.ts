4faaa8a86b6009f6118fd0f492d9166f2e98f0ff
sakamata0 <iskanderboughnimi@gmail.com>
2026-03-17 01:50:00 +0100

    Fixing components model

--- Frontend/src/app/features/landing-page/landing-page.ts
+++ Frontend/src/app/features/landing-page/landing-page.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
+import { HeroSection } from './hero-section/hero-section';
+import { Section1 } from './first-section/section1';
 
 @Component({
   selector: 'app-landing-page',
   imports: [
     Header,
+    HeroSection,
+    Section1
   ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
