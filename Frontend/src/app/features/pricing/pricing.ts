1972981b59c9a1ffb7a7c424f5c2395cc128b281
Ismail Mechkene <imechkene@gmail.com>
2026-03-18 13:16:53 +0100

    feat: update pricing page layout and add steps section component
    - Refactor features section styling and markup
    - Update pricing page template and logic
    - Add new steps section component

--- Frontend/src/app/features/pricing/pricing.ts
+++ Frontend/src/app/features/pricing/pricing.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { MainSection } from './components/main-section/main-section';
 import { FeaturesSection } from './components/features-section/features-section';
+import { StepsSection } from './components/steps-section/steps-section';
 
 @Component({
   selector: 'app-pricing',
   imports: [
     Header,
     MainSection,
-    FeaturesSection
+    FeaturesSection,
+    StepsSection
   ],
   templateUrl: './pricing.html',
   styleUrl: './pricing.css',
